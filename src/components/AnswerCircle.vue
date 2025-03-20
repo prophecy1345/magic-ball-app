13a822858868ee6396688d9a6712a2fab22e990c
prophecy1345 <likeandrewryan@gmail.com>
2025-03-20 17:22:33 +0100

    fix

--- src/components/AnswerCircle.vue
+++ src/components/AnswerCircle.vue
@@ -37,8 +37,6 @@ export default {
   },
   computed: {
     answerWords() {
-      console.log('answerText:', this.answerText);
-      console.log('showAnswer:', this.showAnswer);
       return this.answerText ? this.answerText.split(' ') : [];
     }
   }
